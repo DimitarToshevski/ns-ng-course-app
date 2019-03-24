653e9ac507a148f5f794cd072df81123434c87db
DimitarToshevski <dimitar_toshevski@abv.bg>
2019-03-24 16:47:50 +0200

    add :mode route to challenges so can pass params, subscribing to params in edit challenge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,21 @@ import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-ta
 
 const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'challenges', component: ChallengeTabsComponent,
+    {
+        path: 'challenges',
         children: [
-            { path: 'today', component: TodayComponent, outlet: 'today' },
-            { path: 'current-challenge', component: CurrentChallengeComponent, outlet: 'currentChallenge' },
+            {
+                path: 'tabs',
+                component: ChallengeTabsComponent,
+                children: [
+                    { path: 'today', component: TodayComponent, outlet: 'today' },
+                    { path: 'current-challenge', component: CurrentChallengeComponent, outlet: 'currentChallenge' }
+                ]
+            },
+            { path: ':mode', component: ChallengeEditComponent },
+            { path: '', redirectTo: '/challenges/tabs', pathMatch: 'full'}
         ]
     },
-    { path: 'edit-challenge', component: ChallengeEditComponent}
 ];
 
 @NgModule({
--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PageRoute } from 'nativescript-angular/router';
 
 @Component({
     selector: 'ns-challenge-edit',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./challenge-edit.component.css'],
     moduleId: module.id
 })
-export class ChallengeEditComponent {
+export class ChallengeEditComponent implements OnInit {
+    constructor(private _route: ActivatedRoute, private _pageRoute: PageRoute) { }
+
+    ngOnInit() {
+        // Pages are cached so if navigating back from another page  to this one, ngOnInit wont trigger again
+
+        // this._route.paramMap.subscribe((params)=>{
+        // });
+
+        // Using pageRoute so even if the page has been cached we can get the proper data
+        // It returns the activatedRoute every time when the page is active no matter if taken from cache
+        // Use this approach unless you know that your page wont come from cache(no page follows this one)
+        this._pageRoute.activatedRoute.pipe(
+            switchMap((activatedRoute) => {
+                return activatedRoute.paramMap
+            })
+        ).subscribe((params) => {
+            console.log(params);
+
+        })
+    }
 }
