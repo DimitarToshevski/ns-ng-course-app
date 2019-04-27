e2bcc3d3737bbc0dcc265be050ada8f31a70df67
war_master@bitex.bg <war_master@bitex.bg>
2019-04-27 19:46:25 +0300

    Added Sass, Added theme and theme overrides, Updated current challenge component, Added calendar, Updated styles globally, Added colors sass file

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { ModalDialogService } from 'nativescript-angular/modal-dialog';
 
 import { DayModalComponent } from '../day-modal/day-modal.component';
@@ -7,12 +7,38 @@ import { UIService } from '~/app/shared/ui/ui.service';
 @Component({
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
-    styleUrls: ['./current-challenge.component.common.css','./current-challenge.component.css'],
+    styleUrls: ['./_current-challenge.component.common.scss','./current-challenge.component.scss'],
     moduleId: module.id
 })
-export class CurrentChallengeComponent {
+export class CurrentChallengeComponent implements OnInit {
+
+    weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    days: Array<{ dayInMonth: number, dayInWeek: number }> = [];
+
+    private _currentMonth: number;
+    private _currentYear: number;
 
     constructor(private _modalDialog: ModalDialogService, private _uiService: UIService, private _vcRef: ViewContainerRef) { }
+
+    ngOnInit() {
+        this._currentYear = new Date().getFullYear();
+        this._currentMonth = new Date().getMonth();
+        const daysInMonth = new Date(this._currentYear, this._currentMonth + 1, 0).getDate();
+        
+        for( let i = 1; i < daysInMonth + 1; i++) {
+            const date = new Date(this._currentYear, this._currentMonth, i);
+            const dayInWeek = date.getDay();
+            this.days.push({ dayInMonth: i, dayInWeek: dayInWeek })
+        }
+    }
+
+    getRow(index: number, day: { dayInMonth: number, dayInWeek: number }) {
+        const startRow = 1;
+        const weekRow = Math.floor(index / 7);
+        const firstWeekDayOfTheMonth = new Date(this._currentYear, this._currentMonth, 1).getDay();
+        const irregularRow = day.dayInWeek < firstWeekDayOfTheMonth ? 1 : 0;
+        return startRow + weekRow + irregularRow;
+    }
 
     onChangeStatus() {
         this._modalDialog.showModal(DayModalComponent, {
