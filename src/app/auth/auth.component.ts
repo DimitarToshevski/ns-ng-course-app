85b0d573cb2f14fbf14858fcadb6b35e3b0df431
war_master@bitex.bg <war_master@bitex.bg>
2019-04-28 13:35:06 +0300

    Added auth reactive form, Added challenge edit template form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { TextField } from "tns-core-modules/ui/text-field";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
-  selector: 'ns-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
-  moduleId: module.id,
+    selector: "ns-auth",
+    templateUrl: "./auth.component.html",
+    styleUrls: ["./auth.component.css"],
+    moduleId: module.id
 })
 export class AuthComponent implements OnInit {
+    @ViewChild("passwordEl") passwordEl: ElementRef<TextField>;
+    @ViewChild("emailEl") emailEl: ElementRef<TextField>;
 
-  constructor() { }
+    loginForm: FormGroup;
+    emailControlIsValid = true;
+    passwordControlIsValid = true;
+    isLogin = true;
 
-  ngOnInit() {
-  }
+    get emailCtrl() {
+        return this.loginForm.get("email");
+    }
+    get passwordCtrl() {
+        return this.loginForm.get("password");
+    }
 
+    constructor(private _router: RouterExtensions) {}
+
+    ngOnInit() {
+        this.loginForm = new FormGroup({
+            email: new FormControl(null, {
+                updateOn: "blur",
+                validators: [Validators.required, Validators.email]
+            }),
+            password: new FormControl(null, {
+                updateOn: "blur",
+                validators: [Validators.required, Validators.minLength(6)]
+            })
+        });
+
+        this.emailCtrl.statusChanges.subscribe(status => {
+            this.emailControlIsValid = status === "VALID";
+        });
+
+        this.passwordCtrl.statusChanges.subscribe(status => {
+            this.passwordControlIsValid = status === "VALID";
+        });
+    }
+
+    login() {
+        this.onDone();
+        if (!this.loginForm.valid) {
+            return;
+        }
+        console.log(this.emailCtrl.value, this.passwordCtrl.value);
+        this._router.navigateByUrl("/challenges");
+        this.loginForm.reset();
+        this.emailControlIsValid = true;
+        this.passwordControlIsValid = true;
+    }
+
+    onDone() {
+        this.emailEl.nativeElement.focus();
+        this.passwordEl.nativeElement.focus();
+        this.passwordEl.nativeElement.dismissSoftInput();
+    }
+
+    onSwitch() {
+        this.isLogin = !this.isLogin;
+    }
 }
--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { PageRoute } from 'nativescript-angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { switchMap } from "rxjs/operators";
+import { PageRoute, RouterExtensions } from "nativescript-angular/router";
 
 @Component({
-    selector: 'ns-challenge-edit',
-    templateUrl: './challenge-edit.component.html',
-    styleUrls: ['./challenge-edit.component.scss'],
+    selector: "ns-challenge-edit",
+    templateUrl: "./challenge-edit.component.html",
+    styleUrls: ["./challenge-edit.component.scss"],
     moduleId: module.id
 })
 export class ChallengeEditComponent implements OnInit {
     isCreating = true;
     title: string;
 
-    constructor(private _route: ActivatedRoute, private _pageRoute: PageRoute) { }
+    constructor(
+        private _route: ActivatedRoute,
+        private _pageRoute: PageRoute,
+        private _router: RouterExtensions
+    ) {}
 
     ngOnInit() {
         // Pages are cached so if navigating back from another page  to this one, ngOnInit wont trigger again
@@ -24,16 +28,23 @@ export class ChallengeEditComponent implements OnInit {
         // Using pageRoute so even if the page has been cached we can get the proper data
         // It returns the activatedRoute every time when the page is active no matter if taken from cache
         // Use this approach unless you know that your page wont come from cache(no page follows this one)
-        this._pageRoute.activatedRoute.pipe(
-            switchMap((activatedRoute) => {
-                return activatedRoute.paramMap
-            })
-        ).subscribe((params) => {
-            if(!params.has('mode')) {
-                this.isCreating = true;
-            } else {
-                this.isCreating = params.get('mode') !== 'edit';
-            }
-        })
+        this._pageRoute.activatedRoute
+            .pipe(
+                switchMap(activatedRoute => {
+                    return activatedRoute.paramMap;
+                })
+            )
+            .subscribe(params => {
+                if (!params.has("mode")) {
+                    this.isCreating = true;
+                } else {
+                    this.isCreating = params.get("mode") !== "edit";
+                }
+            });
+    }
+
+    onSubmit(title: string, description: string) {
+        console.log(title, description);
+        this._router.backToPreviousPage();
     }
 }
